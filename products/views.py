e43235d64df37704526e33bde967b447b18b0f6c
Vladimirs Lahiza <vladimirs.lahiza@gmail.com>
2019-05-10 16:50:36 +0100

    products.views updated

--- products/views.py
+++ products/views.py
@@ -2,36 +2,6 @@ from django.views.generic import ListView, DetailView
 from django.shortcuts import render, get_object_or_404, Http404
 from .models import Product
 
-
-class ProductFeaturedListView(ListView):
-    template_name = "products/list.html"
-    def get_queryset(self, *args, **kwargs):
-        return Product.objects.all().featured()
-
-class ProductFeaturedDetailView(DetailView): # class based view
-    queryset = Product.objects.all().featured()
-    template_name = "products/featured-detail.html"
-    # def get_queryset(self, *args, **kwargs):
-    #     return Product.objects.featured()
-
-
-class ProductListView(ListView): # class based view
-    # model = Product // same way that queryset
-    queryset = Product.objects.all()
-    template_name = "products/list.html"
-
-    # def get_context_data(self, *args, **kwargs):
-    #     context = super(ProductListView, self).get_context_data(*args, **kwargs)
-    #     print(context)
-    #     return context
-
-def product_list_view(request): # function based view
-    queryset = Product.objects.all()
-    context = {
-        'object_list': queryset,
-    }
-    return render(request, "products/list.html", context)
-
 class ProductDetailSlugView(DetailView):
     queryset = Product.objects.all()
     template_name = "products/detail.html"
@@ -49,6 +19,37 @@ class ProductDetailSlugView(DetailView):
         except:
             raise Http404("Hmmmm")
         return instance
+
+class ProductListView(ListView): # class based view
+    # model = Product // same way that queryset
+    queryset = Product.objects.all()
+    template_name = "products/list.html"
+
+    # def get_context_data(self, *args, **kwargs):
+    #     context = super(ProductListView, self).get_context_data(*args, **kwargs)
+    #     print(context)
+    #     return context
+
+class ProductFeaturedListView(ListView):
+    template_name = "products/list.html"
+    def get_queryset(self, *args, **kwargs):
+        return Product.objects.all().featured()
+
+class ProductFeaturedDetailView(DetailView): # class based view
+    queryset = Product.objects.all().featured()
+    template_name = "products/featured-detail.html"
+    # def get_queryset(self, *args, **kwargs):
+    #     return Product.objects.featured()
+
+def product_list_view(request): # function based view
+    queryset = Product.objects.all()
+    context = {
+        'object_list': queryset,
+    }
+    return render(request, "products/list.html", context)
+
+
+
 class ProductDetailView(DetailView): # class based view
     # model = Product // same way that queryset
     queryset = Product.objects.all()
@@ -77,6 +78,15 @@ def product_detail_view(request, pk=None, *args, **kwargs): # function based vie
     # else:
     #     raise Http404("Product doesnot exist")
 
+    context = {
+        'object': instance,
+    }
+    return render(request, "products/detail.html", context)
+
+def product_detail_slug(request, slug):
+    instance = Product.objects.get(slug=slug)
+    if instance is None:
+        raise Http404("Product doesnot exist")
     context = {
         'object': instance,
     }
