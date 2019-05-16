a45e73e213aac7f2e3c1e543b640531a442671ea
Vladimirs Lahiza <vladimirs.lahiza@gmail.com>
2019-05-17 00:26:42 +0100

    billing profile created

--- carts/views.py
+++ carts/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render, redirect
 from .models import Cart
 from products.models import Product
 from orders.models import Order
-
+from accounts.forms import LoginForm
+from billing.models import BillingProfile
 def cart_home(request):
     cart_obj, new_obj = Cart.objects.new_or_get(request) # new_or_get from CartManager
     return render(request, "carts/cart_home.html", {"cart": cart_obj})
@@ -30,5 +31,14 @@ def checkout_home(request):
         return redirect("cart_home")
     else:
         order_obj, new_order_obj = Order.objects.get_or_create(cart=cart_obj)
-        print(new_order_obj)
-    return render(request, "carts/checkout.html", {"object": order_obj})
+    user = request.user
+    billing_profile = None
+    login_form = LoginForm()
+    if user.is_authenticated:
+        billing_profile, billing_profile_created = BillingProfile.objects.get_or_create(user=user, email=user.email)
+    context = {
+         "object": order_obj,
+         "billing_profile": billing_profile,
+         "login_form": login_form
+        }
+    return render(request, "carts/checkout.html", context)
