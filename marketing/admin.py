a42c434b7f2ad3ce3010787aa35eff0ff4482cb9
Vladimirs Lahiza <vladimirs.lahiza@gmail.com>
2019-06-06 00:37:54 +0100

    utils done

--- marketing/admin.py
+++ marketing/admin.py
@@ -2,4 +2,16 @@ from django.contrib import admin
 
 from .models import MarketingPreference
 
-admin.site.register(MarketingPreference)
+class MarketingPreferenceAdmin(admin.ModelAdmin):
+    list_display = ['__str__', 'subscribed', 'update']
+    readonly_fields = ['mailchimp_msg', 'mailchimp_subscribed', 'timestamp', 'update']
+    class Meta:
+        model = MarketingPreference
+        fields = ['user',
+                'subscribed',
+                'mailchimp_msg',
+                'mailchimp_subscribed',
+                'timestamp',
+                'updated']
+
+admin.site.register(MarketingPreference, MarketingPreferenceAdmin)
